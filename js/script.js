de008c46bef73a169fcd9364a2b63c0e337c1691
arturo <arturomazzoni@gmail.com>
2020-12-18 10:12:59 +0100

    commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Il software deve chiedere per 5 volte all’utente di inserire un numero.
+// Il programma stampa la somma di tutti i numeri inseriti.
+// Esegui questo programma in due versioni, con il for e con il while
+
+
+// for (var i = 0; i < 5; i++) {
+//   var numero = parseInt(prompt("inserisci un numero"));
+// }
+
+var i = 0;
+while(i < 5) {
+  var numero = parseInt(prompt("inserisci un numero"));
+  i++
+}
